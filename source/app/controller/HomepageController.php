c0ccd39c8db485f3f38937b2b52f92a24b4202d2
eurekasfray <eurekasfray@gmail.com>
2013-11-13 12:38:08 -0400

    Added other test method

--- source/app/controller/HomepageController.php
+++ source/app/controller/HomepageController.php
@@ -8,6 +8,12 @@ class HomepageController
         $msg = "Hello, " . $name;
         return new \Underwear\Component\Response($msg);
     }
+    
+    public function showPage()
+    {
+        $content = "Welcome to the homepage!";
+        return new \Underwear\Component\Response($content);
+    }
   
 }
 
