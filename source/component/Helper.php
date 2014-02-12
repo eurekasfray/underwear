8cc82552e0bb3d89f02483798c5810071c21e576
eurekasfray <eurekasfray@gmail.com>
2014-02-12 19:36:49 -0400

    Small change to trimWhitespace()

--- source/component/Helper.php
+++ source/component/Helper.php
@@ -7,15 +7,15 @@ class Helper
 
     public static function trimWhitespace($subject)
     {
-        define("SPACE"," ");
-        define("HORIZONTAL_TAB","\t");
-        define("VERTICAL_TAB","\v");
-        define("NEWLINE","\n");
-        define("CARRIAGE_RETURN","\r");
-        define("NULL_CHARACTER","\0");
-        define("FORWARD_SLASH","/");
+        $space = " ";
+        $horizontal_tab = "\t";
+        $vertical_tab = "\v";
+        $newline = "\n";
+        $carriage_return = "\r";
+        $null_character = "\0";
+        $forward_slash = "/";
         
-        $result = trim($subject, SPACE . HORIZONTAL_TAB . VERTICAL_TAB . NEWLINE . CARRIAGE_RETURN . NULL_CHARACTER . FORWARD_SLASH );
+        $result = trim( $subject, $space . $horizontal_tab . $vertical_tab . $newline . $carriage_return . $null_character . $forward_slash );
         
         return $result;
         
