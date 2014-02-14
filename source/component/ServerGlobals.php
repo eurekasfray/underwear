d39e73330a4e659a6182e460dc38fe8e62325e0c
eurekasfray <eurekasfray@gmail.com>
2014-02-13 20:28:55 -0400

    Create server globals

--- source/component/ServerGlobals.php
+++ source/component/ServerGlobals.php
@@ -5,15 +5,65 @@ namespace Underwear\Component;
 class ServerGlobals
 {
 
-    // This is in attempts to isolate the app from globals
-
-    public static $server;
+    // This is in attempts to isolate the app from globals. Why? Idk.
     
     public function __construct()
     {
-        foreach ($_SERVER as $key=>$value) {
-            $this->server[$key] = $value;
+    }
+    
+    public function createGetGlobal()
+    {
+        $get = new \Underwear\Component\Bag();
+    
+        foreach ($_GET as $key=>$value) {
+            $get->add($key,$value);
         }
+        
+        return $get;
+    }
+    
+    public function createPostGlobal()
+    {
+        $post = new \Underwear\Component\Bag();
+    
+        foreach ($_POST as $key=>$value) {
+            $post->add($key,$value);
+        }
+        
+        return $post;
+    }
+    
+    public function createServerGlobal()
+    {
+        $server = new \Underwear\Component\Bag();
+    
+        foreach ($_SERVER as $key=>$value) {
+            $server->add($key,$value);
+        }
+        
+        return $server;
+    }
+    
+    public function createFilesGlobal()
+    {
+        $files = new \Underwear\Component\Bag();
+    
+        foreach ($_FILES as $key=>$value) {
+            $files->add($key,$value);
+        }
+        
+        return $files;
+    }
+    
+    public function createCookieGlobal()
+    {
+        $cookie = new \Underwear\Component\Bag();
+    
+        foreach ($_COOKIE as $key=>$value) {
+            $cookie->add($key,$value);
+        }
+        
+        return $cookie;
     }
 
 }
