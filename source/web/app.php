83fa8bba059defa7623abbcc4ea1029eef401ba6
eurekasfray <eurekasfray@gmail.com>
2014-04-20 02:09:56 -0400

    Adjusted app.php to recent changes
    1. Bootstrap.php is now loaded from "/source/core" directory
    
    2. Bootstrap loads the services and components, and now, the core.
    
    3. Bootstrap and Kernel are addressed from under the "Underwear\Core"
    namespace, instead of the "Underwear\Component" namespace where they
    were previously placed before the framework core and framework
    components were separated.
    
    4. Kernel->handle() accepts an HttpRequest, not an AbstractRequest.
    
    5. Did some documentation

--- source/web/app.php
+++ source/web/app.php
@@ -1,24 +1,49 @@
 <?php
 
-// Perfect; needs not any change.
+// app.php: the front controller
+
+// All requests to the server are redirected to this file by the htaccess file.
+// This file, the front controller, is the port of entry for all requests
+// (except when requests are for existing files and directories).
+//
+// The front controller that interacts directly with the client. It receives
+// requests sent by the client, let's the application process the request,
+// lets the application provide back to it a response, and then it sends back
+// the response to the client.
+
+// Get the real path of where this file is located on the server.
 
 define ("ENVIRONMENT_PATH", realpath(dirname("..\..")));
 
+// Require some important files that are needed to get started:
+// the framework config and the boostrap
+
 require_once ENVIRONMENT_PATH . DIRECTORY_SEPARATOR . "config.php";
-require_once COMPONENT_DIRECTORY . DIRECTORY_SEPARATOR . "Bootstrap" . FILE_EXTENSION;
+require_once CORE_DIRECTORY . DIRECTORY_SEPARATOR . "Bootstrap" . FILE_EXTENSION;
 //require_once INCLUDE_DIRECTORY . DIRECTORY_SEPARATOR . "autoload" . FILE_EXTENSION;
 
-use Underwear\Component;
+// Bootstrap the framework core, components, and services
 
-$bootstrap = new \Underwear\Component\Bootstrap();
+$bootstrap = new \Underwear\Core\Bootstrap();
+$bootstrap->loadCore();
 $bootstrap->loadComponents();
 $bootstrap->loadServices();
 
-$kernel = new \Underwear\Component\Kernel();
+// In this paragraph of code below, we facilitates the interaction between
+// the client and the application.
+//
+// To know what the client wants, we need to obtain the HTTP request. The HTTP
+// request contains information about what the client wants. Once we get the
+// HTTP request, we then hand the it to the kernel. The kernel's job here
+// is to dispatch the app controller that corresponds to the request. Once the
+// controller is found and dispatched, at the end of its run, the controller must
+// provide a response (because the application is responsible for creating the
+// responses). The returned response is then sent out to the client.
+
+$kernel = new \Underwear\Core\Kernel();
 $kernel->boot();
-$request = new \Underwear\Component\Request();
-$abstractRequest = $request->synthesize();
-$response = $kernel->handle($abstractRequest);
+$request = new \Underwear\Component\HttpRequest();
+$response = $kernel->handle($request);
 $response->send();
 $kernel->shutdown();
 
