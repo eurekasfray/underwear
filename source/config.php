89eccdba2f76a00419246030234189d09c771327
eurekasfray <eurekasfray@gmail.com>
2014-04-20 01:54:14 -0400

    Added core directory address to config.php

--- source/config.php
+++ source/config.php
@@ -1,6 +1,8 @@
 <?php
 
-// Framework configuration globals. They are applied across the entire framework
+// config.php: the framework configuration file.
+//
+// This file contains framework configuration globals. Configuration globals are applied across the entire framework; this inclusion covers the entire framework environment.
 
 define ("APP_DIRECTORY", ENVIRONMENT_PATH . DIRECTORY_SEPARATOR . "app");
 define ("APP_CONTROLLER_DIRECTORY", APP_DIRECTORY . DIRECTORY_SEPARATOR . "controller");
@@ -9,7 +11,10 @@ define ("APP_CONFIG_DIRECTORY", APP_DIRECTORY . DIRECTORY_SEPARATOR . "config");
 define ("COMPONENT_DIRECTORY", ENVIRONMENT_PATH . DIRECTORY_SEPARATOR . "component");
 define ("INCLUDE_DIRECTORY", ENVIRONMENT_PATH . DIRECTORY_SEPARATOR . "include");
 define ("SERVICE_DIRECTORY", ENVIRONMENT_PATH . DIRECTORY_SEPARATOR . "service");
+define ("CORE_DIRECTORY", ENVIRONMENT_PATH . DIRECTORY_SEPARATOR . "core");
 define ("WEB_DIRECTORY", ENVIRONMENT_PATH . DIRECTORY_SEPARATOR . "web");
+
+// Extension of framework files
 
 define ("FILE_EXTENSION", ".php");
 
