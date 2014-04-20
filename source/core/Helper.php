23eeb149e7798c283e221f18370fed7074fd7d12
eurekasfray <eurekasfray@gmail.com>
2014-04-20 00:57:24 -0400

    Moved Helper class file; object does nothing now
    1. Moved Helper class file from the "/source/component" directory to the
    "/source/core" directory
    
    2. The Helper object now does nothing.

---
+++ source/core/Helper.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Underwear\Core;
+
+// Note:
+//
+// 1. All Helper methods must be static! We do not want to have the pain of instantiating Helper. Helper should always be willing and ready, on any call, to serve our needs. The instantiation of Helper seems very unnecessary and very avoidable.
+
+class Helper
+{    
+    // ...
+}
