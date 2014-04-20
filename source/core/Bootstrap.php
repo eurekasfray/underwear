e6cf7e3152e5d208b69a57b549fd50b8cc281908
eurekasfray <eurekasfray@gmail.com>
2014-04-20 01:21:46 -0400

    Moved Loader class file; added loader for core
    1. Moved Loader class file from the "/source/component" directory to the
    "/source/core" directory.
    
    2. Added Loader->loadCore(). This method loads the core components from
    the "core" directory.

--- source/core/Bootstrap.php
+++ source/core/Bootstrap.php
@@ -1,9 +1,29 @@
 <?php
 
-namespace Underwear\Component;
+// TODO: Make improvements to this. make its methods more modular. there are duplicate style of code in loadComponents(), loadServices(), and loadCore(). that doesn't be. have something kind of loadAll() that does the walk through of all directories or something.
+
+namespace Underwear\Core;
 
 class Bootstrap
 {
+    
+    public function loadCore()
+    {
+        $classes = scandir(CORE_DIRECTORY,0);
+        if (count($classes) == 0) {
+            throw new \Exception("Missing core: Empty directory");
+        }
+        else {
+            foreach ($classes as $file) {
+                if (is_dir($file)) {
+                    continue;
+                }
+                else {
+                    $this->load( CORE_DIRECTORY . DIRECTORY_SEPARATOR . $file );
+                }
+            }
+        }
+    }
     
     public function loadComponents()
     {
@@ -17,7 +37,7 @@ class Bootstrap
                     continue;
                 }
                 else {
-                    $this->load($file);
+                    $this->load( COMPONENT_DIRECTORY . DIRECTORY_SEPARATOR . $file );
                 }
             }
         }
@@ -35,7 +55,7 @@ class Bootstrap
                     continue;
                 }
                 else {
-                    $this->load($file);
+                    $this->load( SERVICE_DIRECTORY . DIRECTORY_SEPARATOR . $file );
                 }
             }
         }
